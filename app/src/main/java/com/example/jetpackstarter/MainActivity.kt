573d01b65f41359ed9afb34f4b2516da4f42593d
Dmitrii Lipin <dimich3d@ya.ru>
2024-12-22 15:53:14 +0100

    rm

--- app/src/main/java/com/example/jetpackstarter/MainActivity.kt
+++ app/src/main/java/com/example/jetpackstarter/MainActivity.kt
@@ -16,32 +16,13 @@ import com.example.jetpackstarter.ui.theme.JetpackStarterTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContent {
             JetpackStarterTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) { 
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetpackStarterTheme {
-        Greeting("Android")
-    }
-}
